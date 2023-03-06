24f3e2b6c06e65ea3e92b67e8828eba4d5013f85
88950 <118199497+88950@users.noreply.github.com>
2023-03-06 19:46:53 +0000

    reorganize tests

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -1,4 +1,5 @@
 # Author: mfuellbier
 # This file is part of python-coinmarketcal.
 #
-# python-coinmarketcal is free software: you can redistrib
+# python-coinmarketcal is free software: you can redistribute it and/or modify
+# it under the terms of the
