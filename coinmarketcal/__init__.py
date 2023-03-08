87e048c1d243ab6ce758a3a4fb1993e5e0278135
88950 <118199497+88950@users.noreply.github.com>
2023-03-08 21:10:29 +0000

    Param tweak

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -2,4 +2,5 @@
 # This file is part of python-coinmarketcal.
 #
 # python-coinmarketcal is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published 
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the
