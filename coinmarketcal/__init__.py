da3b5f2618363e88ee47f5e860da62cfee99e15e
88950 <118199497+88950@users.noreply.github.com>
2023-03-10 11:52:42 +0000

    ENH: Nightly version

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -4,4 +4,6 @@
 # python-coinmarketcal is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later versio
+# (at your option) any later version.
+#
+# python-coinmarketcal is distributed in the hope that it
