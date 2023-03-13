c58f93393508cf5ba126add7ac8c6e5b5b537fb7
88950 <118199497+88950@users.noreply.github.com>
2023-03-13 10:07:39 +0000

    Documentación

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -8,4 +8,5 @@
 #
 # python-coinmarketcal is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# G
