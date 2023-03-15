5e24f691853d5a4956a0599953f6de52459ed7a2
88950 <118199497+88950@users.noreply.github.com>
2023-03-15 17:44:58 +0000

    ath9k_htc: Fix memory leak

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -11,4 +11,5 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have receiv
+# You should have received a copy of the GNU General Public License
+# along with p
