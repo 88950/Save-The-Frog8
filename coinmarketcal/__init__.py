f0856d2fa6dfab03faa913e6e9325fafbf51334e
88950 <118199497+88950@users.noreply.github.com>
2023-03-17 05:41:01 +0000

    use latest version

--- coinmarketcal/__init__.py
+++ coinmarketcal/__init__.py
@@ -12,4 +12,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with python-coinmarketcal.  If not, see <http://www.gnu.org/licenses/
+# along with python-coinmarketcal.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import logging
+logger=logging.getLogge
