de727102eec1927b5b2bc140e0fc32cf384cc278
88950 <118199497+88950@users.noreply.github.com>
2023-03-20 19:42:44 +0000

    Fix option name

--- coinmarketcal/client.py
+++ coinmarketcal/client.py
@@ -27,3 +27,57 @@ class Coinmarketcal:
 
     def __init__(self, id, secret):
         self.id = id
+        self.secret = secret
+
+    @property
+    def token(self):
+        if not getattr(self, '_token', None):
+            self._token = self.get_token(self.id, self.secret)
+        if datetime.datetime.now() > self._token['expires_at']:
+            self._token = self.get_token(self.id, self.secret)
+        return self._token['access_token']
+
+    def get_token(self, id, secret):
+        # Get the id and secret from
+        # https://api.coinmarketcal.com/developer/register
+        payload = {
+            'grant_type': 'client_credentials',
+            'client_id': id,
+            'client_secret': secret}
+        url = "https://api.coinmarketcal.com/oauth/v2/token"
+        try:
+            events =requests.post(url, data=payload)
+            result = json.loads(events.text)
+            result['expires_at'] = (
+                datetime.datetime.now()
+                + datetime.timedelta(seconds=result['expires_in']))
+        except json.decoder.JSONDecodeError:
+            logger.debug("JSONDecodeError")
+            result = []
+        logger.debug(result)
+        return result
+
+    def get_coins(self):
+        payload = {'access_token': self.token}
+        url = "https://api.coinmarketcal.com/v1/coins"
+        try:
+            events =requests.get(url, params=payload)
+            result = json.loads(events.text)
+        except json.decoder.JSONDecodeError:
+            logger.debug("JSONDecodeError")
+            result = []
+        return result
+
+    def get_categories(self):
+        payload = {'access_token': self.token}
+        url = "https://api.coinmarketcal.com/v1/categories"
+        try:
+            events =requests.get(url, params=payload)
+            result = json.loads(events.text)
+        except json.decoder.JSONDecodeError:
+            logger.debug("JSONDecodeError")
+            result = []
+        return result
+
+    def get_events(self, page=None, max=None,
+                  dateRangeStart=None, dateRangeEnd=None,
