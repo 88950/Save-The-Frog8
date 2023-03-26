15031a61b0718e32f6d1e40014c3d17c28d9884e
88950 <118199497+88950@users.noreply.github.com>
2023-03-26 19:35:08 +0000

    Fixing build issues

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ here = os.path.dirname(__file__)
 
 exec(open(os.path.join(here, 'coinmarketcal', 'version.py')).read())
 setup(name = "coinmarketcal",
-    version = version_strin
+    version = version_string, #@UndefinedVariable,
+    description = "Python wrapper f
